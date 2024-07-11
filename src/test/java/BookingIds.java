8100ee2599320a0bebc4ce6c0e143518cc11429e
adilzada <100288857+adilzada@users.noreply.github.com>
2024-07-11 12:52:14 +0400

    jdkjefkj

---
+++ src/test/java/BookingIds.java
@@ -0,0 +1,30 @@
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class BookingIds {
+
+
+    @Test
+    public void getBooking() {
+//        System.out.println("hello testing");
+
+//        given()
+//                .when()
+//                .get("https://restful-booker.herokuapp.com/booking")
+//                .then()
+//                .statusCode(200)
+//                .log().all();
+
+        Response response= given()
+                .when()
+                .get("https://restful-booker.herokuapp.com/booking/");
+        response
+                .then()
+                .statusCode(200);
+
+
+        }
+    }
+
